50bbcd9927c9123dd40155eb0867f4ed5ee9e3d1
zahra <z.sh.moeini1379@gmail.com>
2021-07-06 16:36:50 +0430

    updata-asli-project

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-$('[data-toggle="tooltip"]').tooltip()
+let tabs = document.querySelector(".tabs");
+let tabHeader = tabs.querySelector(".tab-heder");
+let tabBody = tabs.querySelector(".tab-body");
+let tabHeaderElements = tabs.querySelectorAll(".tab-heder > div");
+let tabBodyElements = tabs.querySelectorAll(".tab-body > div");
+
+for(let i=0;i<tabHeaderElements.length;i++){
+    tabHeaderElements[i].addEventListener("click",function(){
+    tabHeader.querySelector(".active").classList.remove("active");
+    tabHeaderElements[i].classList.add("active");
+    tabBody.querySelector(".active").classList.remove("active");
+    tabBodyElements[i].classList.add("active");
+    });
+}
